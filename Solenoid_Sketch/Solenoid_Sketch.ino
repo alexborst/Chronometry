5a9fea716aafa5d425664d400f69b6a991d0fa7c
Alex Borst <alexborst@gmail.com>
2023-01-13 13:56:06 -0500

    Updates

--- Solenoid_Sketch/Solenoid_Sketch.ino
+++ Solenoid_Sketch/Solenoid_Sketch.ino
@@ -13,6 +13,11 @@ int TEMPSENSOR_PIN = 15; // MUST BE ANALOG INPUT
 int [] SolenoidGroup1 = {SOLENOID_PIN_1, SOLENOID_PIN_2, SOLENOID_PIN_3};
 int [] SolenoidGroup2 = {SOLENOID_PIN_4, SOLENOID_PIN_5, SOLENOID_PIN_6};
 int [] SolenoidGroup3 = {SOLENOID_PIN_7, SOLENOID_PIN_8, SOLENOID_PIN_9};
+int timingFactor1 = 7;
+int timingFactor2 = 3;
+int timingFactor3 = 4;
+int checkDelay = 5000;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -26,6 +31,29 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+  int randomInt = GenerateRandomInt(1, 100);
+
+  if (randomInt % timingFactor1 == 0)
+  {
+    TriggerSolenoidGroup(SolenoidGroup1);
+  }
+
+  if (randomInt % timingFactor2 == 0)
+  {
+    TriggerSolenoidGroup(SolenoidGroup2);
+  }
+  
+  if (randomInt % timingFactor3 == 0)
+  {
+    TriggerSolenoidGroup(SolenoidGroup3);
+  }
+
+  delay(checkDelay);
+}
+
+int GenerateRandomInt(int lowerBound, int upperBound)
+{
+  return (int) ((Math.random() * (upperBound - lowerBound)) + lowerBound);
 }
 
 void InitializeSolenoidGroup(int[] solenoidGroup)
